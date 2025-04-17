3abd1b78c9adc0384ff669f0371de08179ab6f80
MojtabaH1 <mojtaba66586@gmail.com>
2025-04-17 10:04:12 +0200

    Initial SpringBoot push testing

---
+++ src/test/java/com/example/LocalZero/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.LocalZero;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
