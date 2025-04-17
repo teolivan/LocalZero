0f48d7db773d8db2b018f07a9e44c654d905c132
teolivan <74550333+teolivan@users.noreply.github.com>
2025-04-17 10:35:53 +0200

    add basic logic. Not complete, just a start of class implementations.

---
+++ frontend/Controller.java
@@ -0,0 +1,54 @@
+package frontend;
+// denna classen är en controller klass. Kan funka som en mediator -- vi uppfyller då Mediator design pattern.
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Objects;
+
+public class Controller{
+    HashMap<String, Integer> users;
+
+    public Controller(){
+        users = new List<User>();
+    }
+
+    String String post(String id) {
+        String response = "";
+        if(id.isEmpty()){
+            System.out.println("id is empty");
+            response = "empty ID";
+        } else {
+
+            if(users.containsKey(id)){
+                System.out.println("User successfully found");
+
+                 // lägg till logik hur man får tag på usern här;
+
+                String response = user.post();
+                if(Objects.equals(response, "success")) {
+                    System.out.println("Successfull post");
+                } else if (Objects.equals(response, "error")) {
+                    System.out.println("Failed");
+                }
+                response = "success";
+            } else {
+                System.out.println("User not found");
+                response = "user not found";
+            }
+            }
+            return response;
+        }
+
+
+    public void login() {
+    }
+
+    public void logout() {
+    }
+
+    public void comment() {
+    }
+
+    public void createChat() {
+    }
+}
---
+++ frontend/PostInterface.java
@@ -0,0 +1,8 @@
+package frontend;
+
+interface PostInterface {
+         String text = "";
+         int chatID = 0;
+         int postID = 0;
+         User poster = null;
+}
