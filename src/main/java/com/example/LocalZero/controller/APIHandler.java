ca2cb91da3b0c916f28e2b7bca43e41185831268
MojtabaH1 <mojtaba66586@gmail.com>
2025-05-08 16:42:17 +0200

    testing api

--- src/main/java/com/example/LocalZero/controller/APIHandler.java
+++ src/main/java/com/example/LocalZero/controller/APIHandler.java
@@ -1,4 +1,21 @@
 package com.example.LocalZero.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
 public class APIHandler {
+
+    @GetMapping("/inits")
+    public ResponseEntity<String> getInits() {
+        return ResponseEntity.ok("List of initiatives");
+    }
+
+
+
+
+
 }
