7d1d6007d4d9c2cd3de0389d5b832a5ec543599f
Rana Noorzadeh <rananoorzadeh@gmail.com>
2025-05-09 11:27:11 +0200

    profile view

--- client/client/src/index.js
+++ client/client/src/index.js
@@ -17,7 +17,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
 
-    <NewAction/>
+    <HomePage/>
     
     {/* <HomePage/>
     <LandingPage/>
