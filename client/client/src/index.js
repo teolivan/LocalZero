abecbe23cd533f47cdd359c8b8d93ee27a1aa5e5
keppangithub <kevinnordkvist@hotmail.com>
2025-05-12 14:25:33 +0200

    next

--- client/client/src/index.js
+++ client/client/src/index.js
@@ -2,15 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import LandingPage from './Signup_Login/page.jsx';
-import InitiativeView from './Initiative_View/page.jsx';
-import InboxPage from './inbox/page.jsx';
+import LandingPage from './app/Signup_Login/page.jsx';
+import InitiativeView from './app/Initiative_View/page.jsx';
+import InboxPage from './app/inbox/page.jsx';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <InboxPage/>
+    <InitiativeView/>
   </React.StrictMode>
 );
 
